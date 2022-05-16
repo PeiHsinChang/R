cc72ec5a04547a3fcfd54ae3d2994ad60008b1db
peihsinchang <peihsinchang@eztravel.com.tw>
2022-05-16 16:07:40 +0800

    更新 readme.md

--- ggplot2.R
+++ ggplot2.R
@@ -1,6 +1,16 @@
+## 20220516 
+## RStudio -> Perferences -> General -> default working directory -> "~/test/R"
+
+## 安裝 RMarkdown 套件
+install.packages("rmarkdown")
+install.packages("knitr")
+
+## 安裝 ggplot2 套件
+install.packages("ggplot2")
+
+
 library(ggplot2)
 addresses = read.csv(file = "addresses.csv")
 addresses
-
 ggplot(mpg, aes(displ, hwy, colour = class)) + 
   geom_point()
